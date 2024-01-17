45101122856f3507248e8aca3409fcad5888efff
Tabi Ledford <tabiletallecx@gmail.com>
2024-01-16 20:46:30 -0600

    started prokect late, somebody help

---
+++ Develop/script.js
@@ -0,0 +1,19 @@
+// Added variables
+var generateButton = document.querySelector("#generate")
+var passwordText = document.querySelector("#password")
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
