039f4908e807c0134776a624808fe34a125d31d5
Tabi Ledford <tabiletallecx@gmail.com>
2024-01-16 22:31:16 -0600

    added resources into read me in addition to passwo

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,69 @@
-// Added variables
+// These are my variables
 var generateButton = document.querySelector("#generate")
 var passwordText = document.querySelector("#password")
 
+///Elements declared in password functionality 
+var password = ''
+var uppercaseCharactersList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var numericCharactersList = '1234567890'
+var specialCharacterList = '!@#$%^&*()-+'
+var lowercaseCharactersList = 'abcdefghijklmnopqrstuvwxyz'
+var possibleCharacterList = ''
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// Function to make random password
+function makePassword() {
+    var passwordLength = prompt('How many characters would you like in your password? (8-128)', '');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    // Password Length Validation / function stops if not a number inbetween 8-128
+    passwordLength = parseInt(passwordLength)
+    if (passwordLength > 128 || passwordLength < 8) {
+        alert('Password length must be between 8 and 128.')
+        return;
+    }
+    
+    if (Number.isNaN(passwordLength) === true) {
+        alert('Character amount must be a number.')
+        return;
+    }
 
-  passwordText.value = password;
+    var specialCharacters = confirm('Would you like special characters?')
+    var numericCharacters = confirm('Would you like numbers?')
+    var lowercaseCharacters = confirm('Would you like lowercase letters?')
+    var uppercaseCharacters = confirm('Would you like uppercase Characters?')
 
+    // Based on confirmations, type of characters are added to "possibleCharacterList" string
+    if (specialCharacters === true) {
+        possibleCharacterList += specialCharacterList
+    }
+
+    if (numericCharacters === true) {
+        possibleCharacterList += numericCharactersList
+    }
+
+    if (lowercaseCharacters === true) {
+        possibleCharacterList += lowercaseCharactersList
+    }
+
+    if (uppercaseCharacters === true) {
+        possibleCharacterList += uppercaseCharactersList
+    }
+
+    // Function doesn't run if not selected 
+    if (specialCharacters === false && numericCharacters === false && lowercaseCharacters === false && uppercaseCharacters === false) {
+        alert('At least one character type must be selected.')
+        return;
+    }
+
+    //
+    for (var i = 0; i < passwordLength; i++) {
+        password += possibleCharacterList.charAt(Math.floor(Math.random() * possibleCharacterList.length));
+    }
+
+    //brings password to text box 
+    passwordText.textContent = password
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// The generating button!
+generateButton.addEventListener("click", makePassword);
+
+// due dates are just suggestions lol
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # passwordgenerator
 Password generator written with javascript
+I included the specifications of the number of characters, lower and uppercase in addition to the special characters. 
+
+
+
+url
+
+
+
+referenced stack overflow 
+https://stackoverflow.com/questions/1497481/javascript-password-generator
+https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript
